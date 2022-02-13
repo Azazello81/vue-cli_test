10706ca091e3f43f938558e95054a32d49a5f820
Max Morichkin <legat-sv@bk.ru>
2022-02-13 23:34:21 +0300

    17-49

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>Работает!</div>
+  <div>
+    <div>
+      <button @click="addLike">Like</button>
+      <button @click="addDislike">Dislike</button>
+    </div>
+    <div>
+      Количество лайков: <strong>{{ likes }}</strong>
+    </div>
+    <div>
+      Количество дизлайков: <strong>{{ dislikes }}</strong>
+    </div>
+  </div>
 </template>
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    },
+  },
+};
+</script>
 <style></style>
